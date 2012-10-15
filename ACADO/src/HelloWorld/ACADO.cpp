ba400983cdae5fe5f474c24400e863da52ab6890
Daniel Josefsson <dannejosefsson@gmail.com>
2012-10-15 20:01:05 +0200

    Added hello world project and added it to the build structure.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

---
+++ ACADO/src/HelloWorld/ACADO.cpp
@@ -0,0 +1,12 @@
+/**
+ * @name	ACADO.cpp
+ * @author	Daniel Josefsson <dannejosefsson@gmail.com>
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <acado_toolkit.hpp>
+
+int main(void) {
+	puts("Hello Acado!!!");
+	return 0;
+}
