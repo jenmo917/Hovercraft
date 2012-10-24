b697f390bed308a365036a19ee45ea0416bd9514
Jens Moser <jenmo917@student.liu.se>
2012-10-24 10:13:59 +0200

    laddar upp i2c master slave kod
    Signed-off-by: Jens Moser <jenmo917@student.liu.se>

---
+++ Arduino/i2cmaster/i2cmaster.ino
@@ -0,0 +1,34 @@
+#include <Wire.h>
+
+#define LED_PIN 13
+#define BUTTON 3
+byte x = 0;
+int val = 0;
+
+void setup()
+{
+  Wire.begin(); // Start I2C Bus as Master
+  pinMode(LED_PIN, OUTPUT);
+  
+  pinMode(BUTTON, INPUT);
+  digitalWrite(BUTTON, HIGH);       // turn on pullup resistors
+
+  digitalWrite(LED_PIN, LOW);
+
+}
+void loop()
+{
+  
+  val = digitalRead(BUTTON);
+
+  Wire.beginTransmission(9); // transmit to device #9
+  if(val)
+  {
+    Wire.write(1);
+  }
+  else
+  {
+    Wire.write(0);
+  }
+  Wire.endTransmission();    // stop transmitting  
+}
