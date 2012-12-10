663823d30650d15958f822feaa0978e7df8c4b9c
Daniel Josefsson <dannejosefsson@gmail.com>
2012-12-10 13:10:45 +0100

    Refactored so that one now can change one algorithm at the time.
    Signed-off-by: Daniel Josefsson <dannejosefsson@gmail.com>

--- Android/remote.control.android/src/remote/control/motorsignals/MotorSignalsDirector.java
+++ Android/remote.control.android/src/remote/control/motorsignals/MotorSignalsDirector.java
@@ -1,5 +1,7 @@
 package remote.control.motorsignals;
 
+import common.files.android.Constants;
+
 public class MotorSignalsDirector
 {
 	private MotorSignalsBuilder builder;
@@ -37,37 +39,119 @@ public class MotorSignalsDirector
 
 	public MotorSignals buildLinearSystem()
 	{
-		return this.builder
-			.buildPitchLin(CONTROLLER_PITCH_MIN, CONTROLLER_PITCH_MAX,
-				CONTROLLER_PITCH_MEAN, CONTROLLER_PITCH_LIN_DEAD_ZONE,
-				CONTROLLER_PITCH_LIN_SCALE)
-			.buildRollLin(CONTROLLER_ROLL_MIN, CONTROLLER_ROLL_MAX,
-				CONTROLLER_ROLL_MEAN, CONTROLLER_ROLL_LIN_DEAD_ZONE,
-				CONTROLLER_ROLL_LIN_SCALE)
-							.getResult();
+		this.buildPitchLin();
+		this.buildRollLin();
+		return this.builder.getResult();
 	}
 
 	public MotorSignals buildLogSystem()
 	{
-		return this.builder
-			.buildPitchLog(CONTROLLER_PITCH_MIN, CONTROLLER_PITCH_MAX,
-				CONTROLLER_PITCH_MEAN, CONTROLLER_PITCH_LOG_DEAD_ZONE,
-				CONTROLLER_PITCH_LOG_SCALE)
-			.buildRollLog(CONTROLLER_ROLL_MIN, CONTROLLER_ROLL_MAX,
-				CONTROLLER_ROLL_MEAN, CONTROLLER_ROLL_LOG_DEAD_ZONE,
-				CONTROLLER_ROLL_LOG_SCALE)
-			.getResult();
+		this.buildPitchLog();
+		this.buildRollLog();
+		return this.builder.getResult();
 	}
 
 	public MotorSignals buildExpSystem()
 	{
-		return this.builder
-			.buildPitchExp(CONTROLLER_PITCH_MIN, CONTROLLER_PITCH_MAX,
-				CONTROLLER_PITCH_MEAN, CONTROLLER_PITCH_EXP_DEAD_ZONE,
-				CONTROLLER_PITCH_EXP_SCALE)
-			.buildRollExp(CONTROLLER_ROLL_MIN, CONTROLLER_ROLL_MAX,
-				CONTROLLER_ROLL_MEAN, CONTROLLER_ROLL_EXP_DEAD_ZONE,
-				CONTROLLER_ROLL_EXP_SCALE)
-			.getResult();
+		this.buildPitchExp();
+		this.buildRollExp();
+		return this.builder.getResult();
+	}
+
+	private MotorSignalsBuilder buildPitchExp()
+	{
+		this.builder.buildPitchExp(CONTROLLER_PITCH_MIN, CONTROLLER_PITCH_MAX,
+			CONTROLLER_PITCH_MEAN, CONTROLLER_PITCH_EXP_DEAD_ZONE,
+			CONTROLLER_PITCH_EXP_SCALE);
+		return this.builder;
+	}
+
+	private MotorSignalsBuilder buildPitchLin()
+	{
+		this.builder.buildPitchLin(CONTROLLER_PITCH_MIN,
+			CONTROLLER_PITCH_MAX, CONTROLLER_PITCH_MEAN,
+			CONTROLLER_PITCH_LIN_DEAD_ZONE, CONTROLLER_PITCH_LIN_SCALE);
+		return this.builder;
+	}
+
+	private MotorSignalsBuilder buildPitchLog()
+	{
+		this.builder.buildPitchLog(CONTROLLER_PITCH_MIN, CONTROLLER_PITCH_MAX,
+			CONTROLLER_PITCH_MEAN, CONTROLLER_PITCH_LOG_DEAD_ZONE,
+			CONTROLLER_PITCH_LOG_SCALE);
+		return this.builder;
+	}
+
+	private MotorSignalsBuilder buildRollExp()
+	{
+		this.builder.buildRollExp(CONTROLLER_ROLL_MIN, CONTROLLER_ROLL_MAX,
+			CONTROLLER_ROLL_MEAN, CONTROLLER_ROLL_EXP_DEAD_ZONE,
+			CONTROLLER_ROLL_EXP_SCALE);
+		return this.builder;
+	}
+
+	private MotorSignalsBuilder buildRollLin()
+	{
+		this.builder.buildRollLin(CONTROLLER_ROLL_MIN, CONTROLLER_ROLL_MAX,
+			CONTROLLER_ROLL_MEAN, CONTROLLER_ROLL_LIN_DEAD_ZONE,
+			CONTROLLER_ROLL_LIN_SCALE);
+		return this.builder;
+	}
+
+	private MotorSignalsBuilder buildRollLog()
+	{
+		this.builder.buildRollLog(CONTROLLER_ROLL_MIN, CONTROLLER_ROLL_MAX,
+			CONTROLLER_ROLL_MEAN, CONTROLLER_ROLL_LOG_DEAD_ZONE,
+			CONTROLLER_ROLL_LOG_SCALE);
+		return this.builder;
+	}
+
+	public MotorSignals changeAlgorithm(MotorSignals ms, String alg, String type)
+	{
+		if (alg.equals(Constants.Broadcast.MotorSignals.Algorithms.PITCH))
+		{
+			this.changePitchAlgorithm(ms, type);
+		}
+		else if (alg.equals(Constants.Broadcast.MotorSignals.Algorithms.ROLL))
+		{
+			this.changeRollAlgorithm(ms, type);
+		}
+		return this.builder.getResult();
+	}
+
+	private void changePitchAlgorithm(MotorSignals ms, String type)
+	{
+		if (type.equals(Constants.Broadcast.MotorSignals.Algorithms.Pitch.EXP))
+		{
+			this.buildPitchExp();
+		}
+		else if (type
+			.equals(Constants.Broadcast.MotorSignals.Algorithms.Pitch.LIN))
+		{
+			this.buildPitchLin();
+		}
+		else if (type
+			.equals(Constants.Broadcast.MotorSignals.Algorithms.Pitch.LOG))
+		{
+			this.buildPitchLog();
+		}
+	}
+
+	private void changeRollAlgorithm(MotorSignals ms, String type)
+	{
+		if (type.equals(Constants.Broadcast.MotorSignals.Algorithms.Roll.EXP))
+		{
+			this.buildRollExp();
+		}
+		else if (type
+			.equals(Constants.Broadcast.MotorSignals.Algorithms.Roll.LIN))
+		{
+			this.buildRollLin();
+		}
+		else if (type
+			.equals(Constants.Broadcast.MotorSignals.Algorithms.Roll.LOG))
+		{
+			this.buildRollLog();
+		}
 	}
 }
